6cb3fd497e7939b563fd6fb0bbd9642e8f1571e2
AbhiRam <144249464+abhir4mxdd@users.noreply.github.com>
2023-10-15 11:07:37 +0530

    Update PublicFlights.java
    Updated constructor

--- .java_files/PublicFlights.java
+++ .java_files/PublicFlights.java
@@ -1,5 +1,7 @@
+import java.time.*;
+
 public class PublicFlights extends FlightRecord{
-    public PublicFlights(String name, int id, int cap) { super(name, id, cap, "Public"); }
+    public PublicFlights(String name, int id, int cap , LocalDateTime dep , LocalDateTime reach) { super(name, id, cap, "Public" , dep , reach); }
 
 //    @Override
 //    void display() {
