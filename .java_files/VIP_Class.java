648186bed885ff402d3c7adf33e4fe4da433d644
AbhiRam <144249464+abhir4mxdd@users.noreply.github.com>
2023-10-15 11:08:04 +0530

    Update VIP_Class.java
    Updated constructor

--- .java_files/VIP_Class.java
+++ .java_files/VIP_Class.java
@@ -1,13 +1,12 @@
+import java.time.*;
+
 public class VIP_Class extends FlightRecord {
 
-    public VIP_Class(String name, int id, int cap) { super(name, id, cap, "VIP"); }
+    public VIP_Class(String name, int id, int cap , LocalDateTime dep , LocalDateTime reach) { super(name, id, cap, "VIP" , dep , reach); }
 
 //    @Override
 //    void display(FlightRecord fr) {
 //
 //
 //    }
-
-
-
 }
