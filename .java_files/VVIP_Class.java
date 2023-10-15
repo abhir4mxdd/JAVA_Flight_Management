2e7d4d3e94d07707b54de075ae972279fd996169
AbhiRam <144249464+abhir4mxdd@users.noreply.github.com>
2023-10-15 11:08:35 +0530

    Update VVIP_Class.java
    Updated constructor

--- .java_files/VVIP_Class.java
+++ .java_files/VVIP_Class.java
@@ -1,8 +1,8 @@
-import java.util.Objects;
+import java.time.*;
 
 public class VVIP_Class extends FlightRecord {
 
-    public VVIP_Class(String name, int id, int cap) { super(name, id, cap, "VVIP"); }
+    public VVIP_Class(String name, int id, int cap , LocalDateTime dep , LocalDateTime reach) { super(name, id, cap, "VVIP" , dep , reach); }
 //    @Override
 //    void display() {
 //
